5d86e3cc4603aa4f2a08adeedacc6ebebfeed06f
Hagsteel <me@hagsteel.com>
2020-10-22 11:22:21 +0200

    fixed test

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,3 @@
-pub fn coords_to_index(x: usize, y: usize, width: usize) -> usize {
-    x + y * width
-}
-
-pub struct Tile {
-}
-
 pub struct TileMap<T> {
     tiles: Vec<T>,
     pub width: usize,
@@ -12,6 +5,10 @@ pub struct TileMap<T> {
 }
 
 impl<T> TileMap<T> {
+    pub fn to_index(&self, p: (usize, usize)) -> usize {
+        p.0 + p.1 * self.width
+    }
+
     pub fn new(width: usize, height: usize) -> Self {
         Self {
             tiles: Vec::with_capacity(width * height),
@@ -32,13 +29,17 @@ impl<T> TileMap<T> {
         &self.tiles[index]
     }
 
-    pub fn by_coords(&self, x: usize, y: usize) -> Option<&T> {
-        let index = coords_to_index(x, y, self.width);
-        Some(self.get(index))
+    pub fn by_coords(&self, p: (usize, usize)) -> Option<&T> {
+        let index = self.to_index(p);
+        if index > self.tiles.len() {
+            None
+        } else {
+            Some(self.get(index))
+        }
     }
 
     /// x1 and y1 has to be smaller than x2 and y2
-    pub fn indices_by_area(&self, p1: (usize, usize), p2: (usize, usize)) -> impl Iterator<Item=(usize, usize)> {
+    pub fn coords_in_area(&self, p1: (usize, usize), p2: (usize, usize)) -> impl Iterator<Item=(usize, usize)> {
         debug_assert!(p1.0 <= p2.0);
         debug_assert!(p1.1 <= p2.1);
         (p1.1..=p2.1).flat_map(move |y| (p1.0..=p2.0).map(move |x| (x, y)))
@@ -51,8 +52,9 @@ mod tests {
 
     #[test]
     fn test_coords_to_index() {
-        let coords = coords_to_index(0, 1, 4);
-        eprintln!("{:?}", coords);
+        let map = TileMap::<usize>::new(4, 2);
+        let index = map.to_index((0, 1));
+        assert_eq!(index, 4);
     }
 
     #[test]
@@ -62,7 +64,7 @@ mod tests {
             0, 1, 2, 3, 
             4, 5, 6, 7
         ]);
-        let t = map.by_coords(2, 1);
+        let t = map.by_coords((2, 1));
         assert_eq!(Some(&6), t);
     }
 
@@ -81,7 +83,7 @@ mod tests {
             0, 1, 2, 3, 
             4, 5, 6, 7
         ]);
-        let tiles = map.indices_by_area(0, 0, 1, 1);
-        assert_eq!(tiles, vec![0, 1, 4, 5]);
+        let tiles = map.coords_in_area((0, 0), (1, 1)).map(|p| map.to_index(p));
+        assert_eq!(tiles.collect::<Vec<_>>(), vec![0, 1, 4, 5]);
     }
 }
